693b7ac7340e8594b803044bcf7c599a97a36936
Kertich <kipsangkertich@gmail.com>
2023-01-20 06:28:51 +0300

    display on the typed text

---
+++ app.js
@@ -0,0 +1,19 @@
+// let a =1
+// let b =2
+
+// setTimeout(function(){
+//     console.log(`Timeout: ` + a);
+// }, 100)
+
+// console.log(b);
+
+function greetings(){
+    console.log(`Hello  + ${name}`)
+
+}
+
+function inputContent(){
+    
+}
+document.getElementById("main").innerHTML=`<div>
+<div>FIST</div></div>`
